c1768bf9dde4883efb1a387507db946e838aaa64
Ine Mariell <imh_93_lfc@hotmail.com>
2016-02-22 17:46:00 +0100

    V03 - interfacet Konstanter og oppdatert klasse Ansatt

--- Innlevering 2/Gjest.java
+++ Innlevering 2/Gjest.java
@@ -1,13 +1,40 @@
 import java.util.Date;
 
 public class Gjest extends Kort {
+	private String navn;
+	private int pin;
+	private Date opprettet;
+	private final long weekInMillis = 604800000;
+	private final Date now = new Date();
 	
-	Date now = new Date();
-	
-	if(opprettet > now+7dager) {
-		return false;
-	}else if (isSperret()==true) {
-		return false;
+	/* konstruktør som ikke tar inn tiden kortet er aktivert */
+	public Gjest(String navn, int pin) {
+		this.navn = navn;
+		this.pin = pin;
+		opprettet.setTime(0);
 	}
-	return true;
+	
+	/* konstruktør som tar inn tiden kortet ble aktivert */
+	public Gjest(String navn, int pin, Date opprettet) {
+		this.navn = navn;
+		this.pin = pin;
+		this.opprettet = opprettet;
+	}
+	
+	public boolean sjekkPIN(int kode) {
+		if (opprettet.getTime() != 0) {
+			if (opprettet.getTime() > now.getTime() + weekInMillis){
+				return false;	//kortet er utgått på tid
+			} else if (opprettet.getTime() < now.getTime()) {
+				return false;	//kortet er ikke aktivert enda
+			}
+		}else if (super.isSperret()==true) {
+			return false;	//kortet er sperret
+		}else if (pin != kode) {
+			return false;	//koden er feil
+		}else{
+		return true;	//ikke utgått på tid, ikke sperret, og riktig kode
+		}
+	}
+	
 }
---
+++ Innlevering 2/Konstanter.java
@@ -0,0 +1,16 @@
+
+public interface Konstanter {
+	
+	public void settFornavn(String navn);
+	public String hentFornavn();
+	
+	public void settEtternavn(String navn);
+	public String hentEtternavn();
+	
+	public void settFulltNavn(String nyttNavn);
+	public String hentFulltNavn();
+	
+	public double beregnKreditt();
+	public double beregnBnus();
+
+}
