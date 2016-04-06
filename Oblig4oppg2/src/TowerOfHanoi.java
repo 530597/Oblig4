869fe997bd40ef230ef3c5f3e6e30ef3e991418c
Ine Mariell <imh_93_lfc@hotmail.com>
2016-04-06 11:31:08 +0200

    Versjon 0.2

--- Oblig4oppg2/src/TowerOfHanoi.java
+++ Oblig4oppg2/src/TowerOfHanoi.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class TowerOfHanoi {
 	
+	private static int teller = 0;
+	
 	public static void main(String[] args) {
 		Scanner leser = new Scanner(System.in);
 		System.out.println("Skriv inn antall skiver: ");
@@ -10,9 +12,11 @@ public class TowerOfHanoi {
 		
 		System.out.println("Flyttingene av skivene er: ");
 		flyttSkiver(ant, 'A', 'B', 'C');
+		System.out.println("Totalt antall flyttinger er " + teller);
 	}
 	
 	public static void flyttSkiver(int n, char fraTaarn, char tilTaarn, char viaTaarn) {
+		teller ++;
 		if (n ==1) {
 			System.out.println("Flytt skive " + n + " fra " + fraTaarn + " til " + tilTaarn);
 		} else {
