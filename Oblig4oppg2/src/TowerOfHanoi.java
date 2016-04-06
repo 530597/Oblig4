d92eb2f2bbd6ec4ce3b4eb48c5667029c37827be
Ine Mariell <imh_93_lfc@hotmail.com>
2016-04-06 11:37:24 +0200

    Versjon 0.3

--- Oblig4oppg2/src/TowerOfHanoi.java
+++ Oblig4oppg2/src/TowerOfHanoi.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class TowerOfHanoi {
 	
 	private static int teller = 0;
+	private static int kallteller = 0;
 	
 	public static void main(String[] args) {
 		Scanner leser = new Scanner(System.in);
@@ -13,16 +14,20 @@ public class TowerOfHanoi {
 		System.out.println("Flyttingene av skivene er: ");
 		flyttSkiver(ant, 'A', 'B', 'C');
 		System.out.println("Totalt antall flyttinger er " + teller);
+		System.out.println("Antall kall til den rekursive metoden er " + kallteller);
 	}
 	
 	public static void flyttSkiver(int n, char fraTaarn, char tilTaarn, char viaTaarn) {
 		teller ++;
+		kallteller ++;
 		if (n ==1) {
 			System.out.println("Flytt skive " + n + " fra " + fraTaarn + " til " + tilTaarn);
 		} else {
 			flyttSkiver(n - 1, fraTaarn, viaTaarn, tilTaarn);
+			kallteller++;
 			System.out.println("Flytt skive " + n + " fra " + fraTaarn + " til " + tilTaarn);
 			flyttSkiver(n - 1, viaTaarn, tilTaarn, fraTaarn);
+			kallteller++;
 		}
 	}
 }
