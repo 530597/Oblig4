063375d84bbcbb9acc058ca60861b62bed31eb93
Ine Mariell <imh_93_lfc@hotmail.com>
2016-04-06 10:39:27 +0200

    versjon 0.1

---
+++ Oblig4oppg1/src/SnuTekst.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SnuTekst {
+	
+	public static void baklengs(String tekst) {
+		int lengde = tekst.length();
+		if (lengde > 0) {
+			System.out.print(tekst.charAt(lengde-1));
+			String nyTekst = tekst.substring(0, lengde-1);
+			baklengs(nyTekst);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner leser = new Scanner(System.in);
+		System.out.println("Skriv inn en tekst: ");
+		String tekst = leser.nextLine();
+		leser.close();
+		
+		System.out.println("Teksten baklengs er: ");
+		baklengs(tekst);
+	}
+}
