1a2436f8af14eea2e2452c180d5b6d7ed90d0140
Ine Mariell <imh_93_lfc@hotmail.com>
2016-04-06 11:01:03 +0200

    Versjon 0.3

--- Oblig4oppg1/SnuTekst.java
+++ Oblig4oppg1/SnuTekst.java
@@ -4,14 +4,17 @@ public class SnuTekst {
 	
 	public static int teller = 0;
 	
-	public static void baklengs(String tekst) {
-		int lengde = tekst.length();
-		if (lengde > 0) {
+	public static void baklengs(String tekst, int siste) {
+		if (siste >= 0) {
 			teller ++;
-			System.out.print(tekst.charAt(lengde-1));
-			String nyTekst = tekst.substring(0, lengde-1);
-			baklengs(nyTekst);
+			System.out.print(tekst.charAt(siste));
+			baklengs(tekst, siste--);
 		}
+	}
+	
+	public static void baklengs(String tekst) {
+		int siste = tekst.length() - 1;
+		baklengs(tekst, siste);
 	}
 
 	public static void main(String[] args) {
@@ -22,10 +25,7 @@ public class SnuTekst {
 		
 		System.out.println("Teksten baklengs er: ");
 		baklengs(tekst);
+		System.out.println("");
 		System.out.println("Teksten har " + teller + " tegn.");
 	}
 }
-
-
-
-
